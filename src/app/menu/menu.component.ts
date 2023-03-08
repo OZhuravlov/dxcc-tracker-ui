b9f0f56e7d0fba75040c31a5e458e4b6112e8095
Oleksandr_Zhuravlov <oleksandr_zhuravlov@epam.com>
2023-03-08 23:20:46 +0200

    Add menu translations

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -11,14 +11,13 @@ export class MenuComponent {
   @Output() featureSelected = new EventEmitter<string>();
 
   constructor(public translate: TranslateService) {
-    translate.addLangs(['en', 'fr']);
-    translate.setDefaultLang('en');
-
     const defaultLang = 'en';
+    translate.addLangs([defaultLang, 'uk']);
+    translate.setDefaultLang(defaultLang);
+
     const browserLang = translate.getBrowserLang() || defaultLang;
     translate.use(browserLang.match(/en|uk/) ? browserLang : defaultLang);
   }
-
 
   show(feature: string) {
     this.featureSelected.emit(feature);
